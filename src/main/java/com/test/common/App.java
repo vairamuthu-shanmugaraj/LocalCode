d2dff316d7be01312d8ec9f6875d781185bb67a7
vairamuthu <vairamuthu-shanmugaraj@gmail.com>
2013-12-22 23:27:13 +0530

    changes. .test 5

--- src/main/java/com/test/common/App.java
+++ src/main/java/com/test/common/App.java
@@ -1,4 +1,4 @@
-package com.mkyong.common;
+package com.test.common;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
--- src/main/java/com/test/common/HelloWorld.java
+++ src/main/java/com/test/common/HelloWorld.java
@@ -1,4 +1,4 @@
-package com.mkyong.common;
+package com.test.common;
 
 /**
  * Hello world!
